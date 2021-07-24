6be054942f128945bfa05cc2274930df81735552
chrismenke45 <chrismenke45@gmail.com>
2021-07-24 12:44:29 -0700

    color switches workand clear works

--- eas.js
+++ eas.js
@@ -8,9 +8,11 @@ while (checkGridSize === false) {
     alert("Grid must be a number between 4 and 100");
     gridSize = prompt("what grid size?");
     }
-}
+};
+
 const container = document.querySelector('div[id="container"')
 container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
+
 let rowNum;
 let columnNum;
 for (i=1; i <= gridSize; i++) {
@@ -22,18 +24,42 @@ for (i=1; i <= gridSize; i++) {
         div.classList.add('cell')
         container.appendChild(div);
     }
-} 
-const sketchable = document.querySelectorAll('div[class="cell"]')
+};
 
-console.log(sketchable)
+const sketchable = document.querySelectorAll('div[class="cell"]');
+
+const blackBtn = document.querySelector(".blackBtn");
+const greyBtn = document.querySelector(".greyBtn");
+const rainbowBtn = document.querySelector(".rainbowBtn");
+const clearBtn = document.querySelector(".clearBtn");
+
+let sketchColor = 'sketchBlack'; //assigns default color to black
+
+greyBtn.addEventListener('click', () => { // this button changes sketch color to grey
+    sketchColor = 'sketchGrey';
+  });
+blackBtn.addEventListener('click', () => { // this button changes sketch color to black
+    sketchColor = 'sketchBlack';
+});
+rainbowBtn.addEventListener('click', () => { // this button changes sketch color to rainbow
+    sketchColor = 'sketchRainbow';
+});
+clearBtn.addEventListener('click', () => { // this button clears etch a sketch
+    sketchable.forEach(div => div.style.removeProperty('background-color'))
+    sketchable.forEach(div => div.classList.remove('sketchBlack'))
+    sketchable.forEach(div => div.classList.remove('sketchGrey'))
+});
+
 sketchable.forEach(div => div.addEventListener('mouseover', () => {
-    div.classList.add('sketched');
-}))
-const black = document.querySelector(".black")
-black.addEventListener('click', () => {
-    sketchable.style.cssText = 'background-color: black';
-});
-const grey = document.querySelector(".grey")
-grey.addEventListener('click', () => {
-    sketchable.style.cssText = 'background-color: red';
-});
+    if (sketchColor === 'sketchBlack' || sketchColor === 'sketchGrey') {
+    div.classList.add(sketchColor);
+    } else if (sketchColor === 'sketchRainbow') {
+        div.style.cssText = `background-color: rgb(${randomColorNumber()}, ${randomColorNumber()}, ${randomColorNumber()})`;
+    }
+}));
+
+function randomColorNumber () {
+    let randomNumber = Math.floor(Math.random() * 255);
+    return randomNumber;
+}
+
