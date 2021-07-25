894f60852fd97daf15088aba3e0a677c3028a521
chrismenke45 <chrismenke45@gmail.com>
2021-07-25 11:57:03 -0700

    fully functional, needs styling

--- eas.js
+++ eas.js
@@ -1,30 +1,38 @@
-gridSize = prompt("what grid size?");
-let checkGridSize = false;
-while (checkGridSize === false) {
-    if (gridSize < 100 && gridSize > 4) {
-        checkGridSize = true;
-        gridSize = Number(gridSize);
-    } else {
-    alert("Grid must be a number between 4 and 100");
-    gridSize = prompt("what grid size?");
-    }
-};
+
+const gridSizeSelector = document.querySelector('.gridSizeSelector');
+let gridSize = gridSizeSelector.value;
+console.log(gridSize);
 
 const container = document.querySelector('div[id="container"')
-container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
 
-let rowNum;
-let columnNum;
-for (i=1; i <= gridSize; i++) {
-    for (j=1; j <= gridSize; j++) {
-        rowNum = i;
-        columnNum = j;
-        let div = document.createElement('div');
-        div.style.cssText = `grid-area: ${rowNum} / ${columnNum} / ${rowNum}+1 / ${columnNum}+1;`
-        div.classList.add('cell')
-        container.appendChild(div);
+function createGrid() {
+    let rowNum;
+    let columnNum;
+    for (i = 1; i <= gridSize; i++) {
+        for (j = 1; j <= gridSize; j++) {
+            rowNum = i;
+            columnNum = j;
+            let div = document.createElement('div');
+            div.style.cssText = `grid-area: ${rowNum} / ${columnNum} / ${rowNum}+1 / ${columnNum}+1;`
+            div.classList.add('cell')
+            container.appendChild(div);
+        }
     }
-};
+}
+
+    container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
+    createGrid();
+
+gridSizeSelector.addEventListener('mouseup', () => {
+    sketchable.forEach(div => div.style.removeProperty('background-color'))
+    sketchable.forEach(div => div.classList.remove('sketchBlack'))
+    sketchable.forEach(div => div.classList.remove('sketchGrey'))
+    gridSize = gridSizeSelector.value;
+    container.style.removeProperty('grid-template-colums');
+    container.style.removeProperty('grid-template-rows');
+    container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`
+    createGrid();
+});
 
 const sketchable = document.querySelectorAll('div[class="cell"]');
 
@@ -37,7 +45,7 @@ let sketchColor = 'sketchBlack'; //assigns default color to black
 
 greyBtn.addEventListener('click', () => { // this button changes sketch color to grey
     sketchColor = 'sketchGrey';
-  });
+});
 blackBtn.addEventListener('click', () => { // this button changes sketch color to black
     sketchColor = 'sketchBlack';
 });
@@ -52,14 +60,19 @@ clearBtn.addEventListener('click', () => { // this button clears etch a sketch
 
 sketchable.forEach(div => div.addEventListener('mouseover', () => {
     if (sketchColor === 'sketchBlack' || sketchColor === 'sketchGrey') {
-    div.classList.add(sketchColor);
+        div.classList.remove('sketchBlack');
+        div.classList.remove('sketchGrey')
+        div.classList.add(sketchColor);
+        div.style.removeProperty('background-color');
     } else if (sketchColor === 'sketchRainbow') {
         div.style.cssText = `background-color: rgb(${randomColorNumber()}, ${randomColorNumber()}, ${randomColorNumber()})`;
     }
 }));
 
-function randomColorNumber () {
+function randomColorNumber() {
     let randomNumber = Math.floor(Math.random() * 255);
     return randomNumber;
 }
+
+
 
